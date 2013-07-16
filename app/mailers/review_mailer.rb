377d33100765a0d53c8e997793b3b2703a26f095
pavelstn <pavelstn@gmail.com>
2013-07-16 17:35:52 +0400

    Update review_mailer.rb

--- app/mailers/review_mailer.rb
+++ app/mailers/review_mailer.rb
@@ -1,5 +1,5 @@
 class ReviewMailer < ActionMailer::Base
-  default from: "robot.bender.0001@mail.ru"
+  default from: "*********@mail.ru"
 
   def welcome_email(phone, post_id, date)
     @phone = phone
@@ -8,10 +8,4 @@ class ReviewMailer < ActionMailer::Base
     mail(:to => "send@send.smsc.ru", :subject => "")
   end
 
-#  def notify_review(phone, post_id, date)
-# 	@phone = phone
-#    @post_id = post_id
-#    @date = date
-#    mail(:to => "send@send.smsc.ru", :subject => "")
-#  end
 end
